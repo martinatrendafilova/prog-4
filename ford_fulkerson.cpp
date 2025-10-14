1dbb2d1fefd26d8571810ed3d920faee6f6ff9eb
martinatrendafilova <martinatrendafilova07@gmail.com>
2025-10-14 09:45:21 +0200

    Domasna zadaca za  14.10.2025

---
+++ ford_fulkerson.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int bfs(int n, int s, int t, vector<vector<int>> &adjList, vector<vector<int>> &c, vector<int> &p) {
+    for (int i = 0; i < n; i++) p[i] = -1;
+    p[s] = -2;
+    queue<pair<int,int>> q;
+    q.push(make_pair(s, INT_MAX));
+
+    while (!q.empty()) {
+        pair<int,int> cur = q.front();
+        q.pop();
+        int u = cur.first;
+        int flow = cur.second;
+
+        for (int i = 0; i < adjList[u].size(); i++) {
+            int v = adjList[u][i];
+            if (p[v] == -1 && c[u][v] > 0) {
+                p[v] = u;
+                int newFlow = min(flow, c[u][v]);
+                if (v == t) return newFlow;
+                q.push(make_pair(v, newFlow));
+            }
+        }
+    }
+    return 0;
+}
+
+int fordFulkerson(int n, int s, int t, vector<vector<int>> &adjList, vector<vector<int>> c) {
+    vector<int> p(n);
+    int totalFlow = 0;
+    int newFlow;
+
+    while ((newFlow = bfs(n, s, t, adjList, c, p))) {
+        totalFlow += newFlow;
+        int v = t;
+        while (v != s) {
+            int u = p[v];
+            c[u][v] -= newFlow;
+            c[v][u] += newFlow;
+            v = u;
+        }
+        cout << " Promeneti kapaciteti:\n";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (c[i][j] > 0)
+                    cout << i << " -> " << j << " : " << c[i][j] << endl;
+            }
+        }
+        cout << endl;
+    }
+
+    return totalFlow;
+}
+
+int main() {
+    int n, m, s, t;
+    cin >> n >> m >> s >> t;
+    vector<vector<int>> adjList(n);
+    vector<vector<int>> c(n, vector<int>(n, 0));
+
+    for (int i = 0; i < m; i++) {
+        int u, v, cap;
+        cin >> u >> v >> cap;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+        c[u][v] = cap;
+    }
+
+    cout << fordFulkerson(n, s, t, adjList, c) << endl;
+    return 0;
+}
